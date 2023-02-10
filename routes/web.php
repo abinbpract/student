cdfc0ae32d296f0c6f2decfe41ca723c38ba2009
abinbpract <abin@bpract.com>
2023-02-10 12:14:44 +0530

    route updated

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 });
 Route::resource('students',StudentController::class);
 Route::resource('students',StudentController::class);
+Route::resource('students',StudentController::class);
