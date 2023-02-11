869e304af61d90d8e3d9c5ef77b43df1bcba5dbd
abinbpract <abin@bpract.com>
2023-02-11 17:40:19 +0530

    filter updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,31 @@
 <form action="" method="GET">
-<input type="text" name="name" id="name" placeholder="name" @if(request()->name!="") value="{{request()->name}}" @endif>
-<input type="text" name="email" id="email" placeholder="email" @if(request()->email!="") value="{{request()->email}}" @endif>
+<input type="text" name="name" id="name" placeholder="name" @if(request('name')!="") value="{{request()->name}}" @endif>
+<input type="text" name="email" id="email" placeholder="email" @if(request('email')!="") value="{{request()->email}}" @endif>
 <label for="">active</label>
 <select name="active" id="active">
     <option value="">select</option>
-    <option value="yes" @if(request()->active=='yes') selected @endif > yes</option>
-    <option value="no" @if(request()->active=='no') selected @endif >no</option>
+    <option value="yes" @if(request('active')=='yes') selected @endif > yes</option>
+    <option value="no" @if(request('active')=='no') selected @endif >no</option>
 </select>
 <label for="">age</label>
 <select name="age_operator" id="age_operator">
     <option value="">select</option>
-    <option value="<" @if(request()->age_operator=='<') selected @endif> < </option>
-    <option value=">" @if(request()->age_operator=='>') selected @endif> > </option>
-    <option value="=" @if(request()->age_operator=='=') selected @endif> = </option>
+    <option value="<" @if(request('age_operator')== '<' ) selected @endif> < </option>
+    <option value=">" @if(request('age_operator')== '>' ) selected @endif> > </option>
+    <option value="=" @if(request('age_operator')== '=' ) selected @endif> = </option>
+    <option value="<=" @if(request('age_operator')=='<=') selected @endif> <= </option>
+    <option value=">=" @if(request('age_operator')=='>=') selected @endif> >= </option>
 </select>
+
 <select name="age" id="age">
     <option value="">select</option>
     @for($i=1;$i<=100;$i++)
-    <option value="{{$i}}" @if(request()->age=='{{$i}}') selected @endif> {{$i}} </option>
+    <option value="{{$i}}" @if(request('age')==$i) selected @endif>{{$i}} </option>
     @endfor
-</select>
-<input type="radio" name="gender" id="gender" value="male" @if(request()->gender=='male') checked @endif>male
-<input type="radio" name="gender" id="gender" value="female" @if(request()->gender=='female') checked @endif>female
+</select>   
+
+<input type="radio" name="gender" id="gender" value="male" @if(request('gender')=='male') checked @endif>male
+<input type="radio" name="gender" id="gender" value="female" @if(request('gender')=='female') checked @endif>female
 <button type="submit">submit</button>
 <a href="{{route('students.index')}}">reset</a>
 </form>
